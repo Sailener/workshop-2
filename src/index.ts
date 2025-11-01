e6db1629043059df7fc2545d4fa3c6119727673d
Ubuntu <ubuntu@DESKTOP-2UFMD59.localdomain>
2025-11-01 18:41:51 +0000

    feat!: change add signature to accept number[]

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ export type { Config, LogLevel };
 
 /** Мінімальні утиліти без суворої типізації */
 
-export function add(a: number, b: number): number {
-  return a + b;
+export function add(values: number[]): number {
+  return values.reduce((acc, x) => acc + x, 0);
 }
 
 export function capitalize(s: string): string {
