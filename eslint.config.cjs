a5fc1fd93783bd2697ae2d922cb8749d4035d95d
Ubuntu <ubuntu@DESKTOP-2UFMD59.localdomain>
2025-11-01 14:54:49 +0000

    feat: basic utils with any (add, capitalize)

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,11 +2,12 @@ const js = require('@eslint/js');
 const ts = require('@typescript-eslint/eslint-plugin');
 const tsParser = require('@typescript-eslint/parser');
 const prettier = require('eslint-config-prettier');
+const globals = require('globals');
 
 module.exports = [
-  { ignores: ['**/*.cjs'] }, // игнорируем конфиги в CJS
-  js.configs.recommended, // базовые правила JS
-  prettier, // отключаем конфликты с prettier
+  { ignores: ['**/*.cjs'] },
+  js.configs.recommended,
+  prettier,
   {
     files: ['**/*.ts'],
     languageOptions: {
@@ -15,13 +16,17 @@ module.exports = [
         ecmaVersion: 'latest',
         sourceType: 'module',
       },
+      globals: {
+        ...globals.node,
+      },
     },
     plugins: {
       '@typescript-eslint': ts,
     },
     rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'warn',
-      'no-unused-vars': 'warn',
     },
   },
 ];
